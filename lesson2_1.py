65135dc5506a018c4b8f0bf9cf7dde0bd587bbe6
Anna Rubanik <arubanik@ukr.net>
2025-10-05 12:16:43 +0200

    lesson 2 exercise1,2

---
+++ lesson2_1.py
@@ -0,0 +1,13 @@
+print("Завдання 1")
+user_input = input("input a four-digital number:")
+number = int (user_input)
+digital1 = number // 1000
+digital2 = (number // 100) % 10
+digital3 = (number // 10) % 10
+digital4 = number % 10
+print (digital1)
+print(digital2)
+print(digital3)
+print(digital4)
+
+
--- lesson2_2.py
+++ lesson2_2.py
@@ -1,16 +1,4 @@
-print("Завдання 1")
-user_input = input("input a four-digital number:")
-number = int (user_input)
-digital1 = number // 1000
-digital2 = (number // 100) % 10
-digital3 = (number // 10) % 10
-digital4 = number % 10
-print (digital1)
-print(digital2)
-print(digital3)
-print(digital4)
 print("Завдання 2")
-
 user_input2 = input("input a 5-digital number:")
 number2 = int(user_input2)
 digital1 = number2 % 10
